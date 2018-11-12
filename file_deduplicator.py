7780b462b8870bc215f663529d376b069a420b0e
Jay Telles <jaytelles24@gmail.com>
2018-11-12 00:15:49 -0500

    get md5s now and started to get a function to parse out duplicates

--- file_deduplicator.py
+++ file_deduplicator.py
@@ -1,23 +1,43 @@
 import os
+import hashlib
+from functools import partial
 
 
-
-
-
+#from https://stackoverflow.com/questions/7829499/using-hashlib-to-compute-md5-digest-of-a-file-in-python-3
+def md5sum(filename):
+    with open(filename, mode='rb') as f:
+        d = hashlib.md5()
+        for buf in iter(partial(f.read, 128), b''):
+            d.update(buf)
+    return d.hexdigest()
 
 def build_file_dict(file_dir):
 	file_dict = dict()
-	files = os.listdir(file_dir)
-	print(files)
+	filenames = os.listdir(file_dir)
+	
+	#should probs be a lambda
+	for x in range(0, len(filenames)):
+		filenames[x] = file_dir + filenames[x]
+	
+	for filename in filenames:
+		file_dict[filename] = md5sum(filename)
+	print(len(file_dict))
 	
 	return file_dict
 
+def parse_out_dups(file_dict):
+	for key in file_dict.keys()
+	
 
+	return file_dict
+
+	
 
 def main():
-	file_dir = "./test_pics/"
-	build_file_dict(file_dir)
-
+	in_file_dir = "./test_pics/"
+	out_file_dir = "./test_final"
+	file_dict = build_file_dict(file_dir)
+	parse_out_dups(file_dict)
 
 
 if __name__ == "__main__":
